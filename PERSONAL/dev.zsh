738de0e8f9c5bf8cd91afaee2bde3102cb69d90a
m-balcewicz <martin.balcewicz@rockphysics.org>
2025-07-10 14:46:54 +0200

    refactor dev.zsh: clean up NVM and Conda setup for improved readability

--- PERSONAL/dev.zsh
+++ PERSONAL/dev.zsh
@@ -1,3 +1,10 @@
+# ---------------------------------------
+# Filename: dev.zsh
+# Author: Martin Balcewicz
+# Date: June 2025
+# Description: Development-related aliases and functions.
+# ---------------------------------------
+
 # -------------------------------------------------------
 # Development-related aliases and functions
 # -------------------------------------------------------
@@ -13,26 +20,9 @@ alias gb="git branch"
 alias gco="git checkout"
 
 # Node Version Manager setup
-brew_path="/opt/homebrew/bin"
-brew_opt_path="/opt/homebrew/opt"
-nvm_path="$HOME/.nvm"
-
-export NVM_DIR="${nvm_path}"
-[ -s "${brew_opt_path}/nvm/nvm.sh" ] && \. "${brew_opt_path}/nvm/nvm.sh"
-[ -s "${brew_opt_path}/nvm/etc/bash_completion.d/nvm" ] && \. "${brew_opt_path}/nvm/etc/bash_completion.d/nvm"
-
-# Conda setup
-__conda_setup="$('/Users/martin/opt/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/Users/martin/opt/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/Users/martin/opt/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/Users/martin/opt/miniconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
 
 # LM Studio CLI
 export PATH="$PATH:/Users/martin/.lmstudio/bin"
